02f928a3b352a11d7473e952bdb59a162ca6f976
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-09-06 10:23:25 +0300

    Setup cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 import connectDB from './config/db'
 import { notFound, errorHandler } from './middleware/errorMiddleware'
@@ -16,6 +17,10 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
+app.use(cors({
+  credentials: true,
+  origin: process.env.CLIENT_URL
+}))
 
 app.use('/api/users', userRoutes)
 
