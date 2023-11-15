55b27acfdbd5b548d03a496aea699d65ce5394d5
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-11-15 17:59:50 +0200

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -21,23 +21,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-/* app.use(
-  session({
-    secret: process.env.SESSION_SECRET || 'sdfhs7dn45nasd',
-    resave: true,
-    saveUninitialized: false,
-    cookie: {
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // must be 'none' to enable cross-site delivery
-      secure: process.env.NODE_ENV === 'production', // must be true if sameSite='none'
-    }
-  })
-); */
-
 app.use(cors({
   credentials: true,
   origin: process.env.CLIENT_URL
 }))
-// app.set('trust proxy', 1)
 
 app.use('/api/users', userRoutes)
 app.use('/api/game', gameRoutes)
